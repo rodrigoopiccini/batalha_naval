ea1eeaa4ae837f039d8b60c48a8e2658ab9eba96
João Andrade <joaoma@al.insper.edu.br>
2023-05-03 15:02:52 -0300

    1

---
+++ Define_Posições.py
@@ -0,0 +1,19 @@
+def define_posicoes (linha, coluna, orientacao, tamanho):
+    lista = []
+
+    if orientacao == 'vertical':
+        for i in range(tamanho):
+            posicao = []
+            posicao.append(linha+i)
+            posicao.append(coluna)
+            lista.append(posicao)
+
+    if orientacao == 'horizontal':
+        for i in range(tamanho):
+            posicao = []
+            posicao.append(linha)
+            posicao.append(coluna+i)
+            lista.append(posicao)
+
+
+    return lista
