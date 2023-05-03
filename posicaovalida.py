27797457c6144f82f761f490809f0875243073c2
rodrigoopiccini <rodrigopiccini12@gmail.com>
2023-05-03 15:19:00 -0300

    6

---
+++ posicaovalida.py
@@ -0,0 +1,22 @@
+def posicao_valida (frota, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes (linha, coluna, orientacao, tamanho)
+
+    if orientacao == 'vertical' and 10 - linha < tamanho:
+        return False
+    
+    if orientacao == 'horizontal' and 10 - coluna < tamanho:
+        return False
+    
+
+    for posicao in posicoes:
+        linha = posicao[0]
+        coluna = posicao[1]
+
+        for tipo, posicoes in frota.items():
+            for posicao in posicoes:
+                for linha_frota, coluna_frota in posicao:
+                    if linha == linha_frota and coluna == coluna_frota:
+                        return False
+                    
+    return True
+
